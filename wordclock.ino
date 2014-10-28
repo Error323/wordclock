c3b1ac1ab9f014a310729e9c0d9781f295b03e34
Error323 <folkerthuizinga@gmail.com>
2014-10-28 12:49:48 +0100

    animatrix

--- wordclock.ino
+++ wordclock.ino
@@ -21,11 +21,9 @@
 #define LED_PIN   7 // Matrix led pin
 #define SIZE     10 // Matrix board size x size
 #define FPS      40 // Frames per second to achieve
-#define MIN_ATIME 1 // Minimal animation time in sec
-#define MAX_ATIME 3 // Maximal animation time in sec
 
 /* Globals */
-static uint32_t color = Adafruit_NeoPixel::Color(200, 0, 0);
+static uint32_t color = Adafruit_NeoPixel::Color(200, 200, 200);
 
 /** @brief the hardware led matrix */
 static Adafruit_NeoPixel led_matrix(SIZE*SIZE, LED_PIN);
@@ -54,56 +52,67 @@ uint8_t idx(int i, int j)
 
 void ani_matrix(const uint32_t activated)
 {
-  static uint8_t fall_time[SIZE];
-  static uint8_t start_time[SIZE];
-  static uint8_t time = 0;
+  static uint8_t speed[SIZE]; // speed in fps
+  static uint8_t start[SIZE]; // start time in frames
+  static uint8_t frame[SIZE]; // current frame
+  static uint8_t wsize[SIZE]; // current frame
   static uint8_t done = 0;
+  static uint8_t time = 0;
   static uint32_t prev_activated = 0ul;
   static uint32_t tmp = 0ul;
-  uint8_t i, j;
+  uint8_t i, j, c;
 
-  if (activated != prev_activated)
+  if (activated != tmp)
   {
     for (i = 0; i < SIZE; i++)
     {
-      fall_time[i] = random(MIN_ATIME*FPS, MAX_ATIME*FPS);
-      start_time[i] = random(0, fall_time[i]-FPS);
+      speed[i] = random(1, 6);
+      start[i] = random(0, 10);
+      wsize[i] = random(5, SIZE);
+      frame[i] = 0;
     }
     done = 0;
     time = 0;
-    tmp = prev_activated;
-    prev_activated = activated;
+    prev_activated = tmp;
+    tmp = activated;
   }
 
   else if (done < SIZE)
   {
-    done = 0;
     for (i = 0; i < SIZE; i++)
     {
-      if (fall_time[i] < time)
-        done++;
-
       for (j = 0; j < SIZE; j++)
       {
-        if (time > start_time[j]+i && wc::wordclock[i*SIZE+j]&activated)
+        // perform animation tick
+        if (frame[j] >= start[j] + i && frame[j] < wsize[j] + start[j] + i)
         {
-          color = 200;
-          led_matrix.setPixelColor(idx(i, j), 0, color, 0);
+          if (frame[j] > start[j]+i && wc::wordclock[i*SIZE+j] & activated)
+            led_matrix.setPixelColor(idx(i,j), color);
+          else
+          {
+            c = round(normal(frame[j]-i-start[j], wsize[j]-3) * 255);
+            led_matrix.setPixelColor(idx(i,j), c, c, c);
+          }
         }
-        else if (time < start_time[j]+i && wc::wordclock[i*SIZE+j]&tmp)
-        {
-          continue;
-        }
-        else if (time >= start_time[j]+i)
-        {
-          color = round(normal(time-start_time[j]-i, fall_time[j])*255);
-          led_matrix.setPixelColor(idx(i, j), 0, color, 0);
-        }
+        // leave (prev)activated as is, turn rest off
         else
         {
-          led_matrix.setPixelColor(idx(i, j), 0);
+          if (wc::wordclock[i*SIZE+j] & (prev_activated|activated))
+            continue;
+          else
+            led_matrix.setPixelColor(idx(i,j), 0);
         }
       }
+    }
+
+    // increase counters, check if done
+    done = 0;
+    for (uint8_t i = 0; i < SIZE; i++)
+    {
+      if (frame[i] > wsize[i] + start[i] + SIZE)
+        done++;
+      if (time % speed[i] == 0)
+        frame[i]++;
     }
     time++;
   }
@@ -113,8 +122,8 @@ void setup()
 {
   Wire.begin();
   rtc.begin();
-  Serial.begin(9600);
   led_matrix.begin();
+  Serial.begin(9600);
 
   pinMode(B1_PIN, INPUT);
   pinMode(B2_PIN, INPUT);
